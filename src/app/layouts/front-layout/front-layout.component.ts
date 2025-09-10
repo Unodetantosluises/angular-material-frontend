2c796a3bb515b583c971695fb8cb834647e71dd9
Luis Diaz <luisantoniodiaz97@gmail.com>
2025-09-10 03:00:14 -0600

    update: header added

--- src/app/layouts/front-layout/front-layout.component.ts
+++ src/app/layouts/front-layout/front-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { HeroComponent } from '../../components/hero/hero.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-front-layout',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, HeroComponent, FooterComponent],
   templateUrl: './front-layout.component.html',
   styleUrl: './front-layout.component.css'
 })
