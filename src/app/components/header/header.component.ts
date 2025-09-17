3b0d16de3f93ea7b5778e9164380d2c3bdb93bc6
Luis Diaz <luisantoniodiaz97@gmail.com>
2025-09-16 19:15:38 -0600

    update: new icon button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
